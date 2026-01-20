eb3ea2ed0816914cee185b1204ecd2d0c34e4d89
elitegamer859650-lab <elitegamer859650@gmail.com>
2026-01-20 17:08:34 +0500

    Create page.js

---
+++ page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: '#000814', color: '#00f2ff', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', fontFamily: 'sans-serif' }}>
+      <h1>Paradox Mail</h1>
+      <p>Secure. Futuristic. Private.</p>
+      <img src="/icon.png" alt="Paradox Logo" style={{ width: '100px', marginTop: '20px' }} />
+    </div>
+  );
+}
