aefb4129ce6e7147a1b534c9c3ca50ea5946a837
elitegamer859650-lab <elitegamer859650@gmail.com>
2026-01-20 17:22:49 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,4 +6,15 @@ export default function Home() {
       <img src="/icon.png" alt="Paradox Logo" style={{ width: '100px', marginTop: '20px' }} />
     </div>
   );
+}import fs from 'fs';
+import path from 'path';
+
+export default function Home() {
+  // Ye line aapki index.html file ko read karegi
+  const filePath = path.join(process.cwd(), 'index.html');
+  const htmlContent = fs.readFileSync(filePath, 'utf8');
+
+  return (
+    <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+  );
 }
